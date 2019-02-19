f096b3d88ca1f91056cf16d25d13bf9d67e93716
LaboratorioU005_24 <LaboratorioU005_24@LaboratorioU005>
2019-02-19 08:48:54 -0600

    Agregamos metodo Bressenham

--- src/Pizarra2.java
+++ src/Pizarra2.java
@@ -18,6 +18,7 @@ import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
 public class Pizarra2 extends javax.swing.JFrame {
@@ -27,11 +28,13 @@ public class Pizarra2 extends javax.swing.JFrame {
      */
     Point   p1, p2;
     boolean bDibujar;
-    JPanel  panel1;
+    
     Raster  raster;
    
     int ancho = 640;
     int alto  = 480;
+    
+    boolean firstTime=true;    
             
     public Pizarra2() {      
         p1 = new Point();
@@ -43,31 +46,33 @@ public class Pizarra2 extends javax.swing.JFrame {
         this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         
         this.setLayout(new BorderLayout());
-       
-        panel1 = new JPanel();
-        panel1.setSize(ancho, alto);
+     
         
-        panel1.addMouseListener(new MouseAdapter(){
+        this.addMouseListener(new MouseAdapter(){
                                         @Override
                                         public void mouseClicked(MouseEvent evt) {
                                                 jPanel1MouseClicked(evt);
                                         }                               
-                                });
-        
-        this.add(panel1,BorderLayout.CENTER);
-       
+                                });        
+        this.setBackground(Color.white);
         this.setVisible(true);
+        
     }
     
     @Override
     public void update(Graphics g){
-         paint(g);
+        paint(g);
     }
 
-    @Override
+    
     public void paint(Graphics g){        
+        if( firstTime )
+        {   
+            firstTime = false;
+            super.paint(g);
+        }
         
-        Image output = raster.toImage(this);
+        Image output =  raster.toImage(this);
         g.drawImage(output, 0, 0, this);        
     }
     
@@ -100,9 +105,73 @@ public class Pizarra2 extends javax.swing.JFrame {
             }
         }
     }
+    
+    public void lineaMejorada(int x0, int y0, int x1, int y1, Color color) {
+        int pix = color.getRGB();
+        int dx = x1 - x0;  int dy = y1 - y0;
+        raster.setPixel(pix, x0, y0);
+        if (Math.abs(dx) > Math.abs(dy)) {     // inclinacion < 1
+            float m = (float) dy / (float) dx; // calcular inclinacion
+            float b = y0 - m*x0;
+            dx = (dx < 0) ? -1 : 1;
+            while (x0 != x1) {
+                x0 += dx;
+                raster.setPixel(pix, x0, Math.round(m*x0 + b));
+            }
+        } else
+        if (dy != 0) {                         // inclinacion >= 1
+            float m = (float) dx / (float) dy; // Calcular inclinacion
+            float b = x0 - m*y0;
+            dy = (dy < 0) ? -1 : 1;
+            while (y0 != y1) {
+                y0 += dy;
+                raster.setPixel(pix, Math.round(m*y0 + b), y0);
+            }
+        }
+    }
 
-    private void dibujarLinea(Point _p1, Point _p2, Color color) {                        
-             lineaSimple(_p1.x,_p1.y,_p2.x,_p2.y,color);
+    public void lineFast(int x0, int y0, int x1, int y1, Color color) {
+        int pix = color.getRGB();
+        int dy = y1 - y0;  int dx = x1 - x0;  int stepx, stepy;
+        if (dy < 0) { dy = -dy;  stepy = -raster.width; } else { stepy = raster.width; }
+        if (dx < 0) { dx = -dx;  stepx = -1; } else { stepx = 1; }
+        dy <<= 1;   dx <<= 1;
+        y0 *= raster.width;  y1 *= raster.width;   raster.pixel[x0+y0] = pix;
+        if (dx > dy) {
+            int fraction = dy - (dx >> 1);
+            while (x0 != x1) {
+                if (fraction >= 0) {
+                    y0 += stepy; fraction -= dx;
+                }
+                x0 += stepx;  fraction += dy;
+                raster.pixel[x0+y0] = pix;
+            }
+        } else {
+            int fraction = dx - (dy >> 1);
+            while (y0 != y1) {
+                if (fraction >= 0) {
+                    x0 += stepx; fraction -= dy;
+                }
+                y0 += stepy; fraction += dx;
+                raster.pixel[x0+y0] = pix;
+            }
+        }
+    }
+
+    
+    private void dibujarLinea(Point _p1, Point _p2, Color color) {
+             long inicio=0, fin=0;
+             inicio = System.nanoTime();
+             lineaMejorada(_p1.x,_p1.y,_p2.x,_p2.y,color);
+             fin    = System.nanoTime();
+             
+             System.out.printf("Tiempo transcurrido, simple: %d\n",(fin-inicio));
+             
+             inicio = System.nanoTime();
+             lineFast(_p1.x,_p1.y,_p2.x,_p2.y,color);
+             fin    = System.nanoTime();
+             
+             System.out.printf("Tiempo transcurrido, fast  : %d\n",(fin-inicio));             
     }    
     
     private void jPanel1MouseClicked(java.awt.event.MouseEvent evt) {                                     
